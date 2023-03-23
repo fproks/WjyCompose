dacaac882bd6428a9f152543ce0965eb134905a4
fproks <fproks@126.com>
2023-03-23 17:04:17 +0800

    init status bar color

---
+++ app/src/main/java/com/linhos/wjycompose/ui/screen/StudyScreen.kt
@@ -0,0 +1,16 @@
+package com.linhos.wjycompose.ui.screen
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun StudyScreen(){
+    Text("study screen")
+}
+
+@Preview
+@Composable
+fun StudyScreenPreview(){
+    StudyScreen()
+}
--- app/src/main/java/com/linhos/wjycompose/ui/theme/Theme.kt
+++ app/src/main/java/com/linhos/wjycompose/ui/theme/Theme.kt
@@ -1,21 +1,16 @@
 package com.linhos.wjycompose.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
+
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
+    primary = Blue700,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Blue200
 
     /* Other default colors to override
     background = Color.White,
@@ -28,15 +23,11 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun WjyComposeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun WjyComposeTheme( content: @Composable () -> Unit) {
+
 
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
