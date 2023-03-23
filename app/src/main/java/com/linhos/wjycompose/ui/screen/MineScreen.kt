e08998ad025779591954e4d9084d4d21a5068729
fprok <fproks@126.com>
2023-03-23 20:32:14 +0800

    创建3个页面

---
+++ app/src/main/java/com/linhos/wjycompose/ui/screen/MineScreen.kt
@@ -0,0 +1,11 @@
+package com.linhos.wjycompose.ui.screen
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun MineScreen(){
+    Text("my page")
+}
