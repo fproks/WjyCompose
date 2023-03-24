1ff127afcc83922818d4d4d9a9dbcce2c9cc071e
linhos <linhos@126.com>
2023-03-24 22:33:29 +0800

    创建Tab

--- app/src/main/java/com/linhos/wjycompose/ui/screen/StudyScreen.kt
+++ app/src/main/java/com/linhos/wjycompose/ui/screen/StudyScreen.kt
@@ -2,13 +2,10 @@ package com.linhos.wjycompose.ui.screen
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Search
-
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,17 +15,30 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.linhos.wjycompose.ui.components.TopAppBar
+import com.linhos.wjycompose.viewmodel.MainViewModel
 
 @Composable
-fun StudyScreen() {
+fun StudyScreen(viewModel: MainViewModel = viewModel()) {
     Column {
 
-        /*
-        * 学习的标题栏
-        * */
-        TopAppBar(modifier = Modifier.padding(horizontal = 8.dp)) {
+        StudystatusBar()
 
+        CategoryTab(viewModel)
+        TypesRowTab(viewModel)
+
+    }
+}
+
+
+/*
+      * 学习的标题栏
+      * */
+@Composable
+fun StudystatusBar() {
+    TopAppBar(modifier = Modifier.padding(horizontal = 8.dp)) {
+        Row {
             Surface(
                 modifier = Modifier.clip(RoundedCornerShape(16.dp)).weight(1f), //圆角
                 color = Color(0x33FFFFFF),  //30%透明度
@@ -63,10 +73,66 @@ fun StudyScreen() {
 
 
         }
-        Text(modifier = Modifier.align(Alignment.CenterHorizontally), text = "study screen")
     }
 }
 
+/*
+* 分类目录
+* */
+@Composable
+fun CategoryTab(viewModel: MainViewModel = viewModel()) {
+    Row {
+        TabRow(
+            selectedTabIndex = viewModel.studyCategoryIndex,
+            backgroundColor = Color(0x22149EE7),
+            contentColor = Color(0xFF149EE7)
+        ) {
+            viewModel.categorys.forEachIndexed { index, category ->
+                LeadingIconTab(
+                    selected = index == viewModel.studyCategoryIndex,
+                    onClick = { viewModel.updateStudyCategoryIndex(index) },
+                    icon = { Icon(imageVector = category.icon, contentDescription = null) },
+                    text = {
+                        Text(
+                            text = category.title,
+                            fontSize = 14.sp
+                        )
+                    },
+                    selectedContentColor = Color(0xFF149EE7),
+                    unselectedContentColor = Color(0xFF666666)
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TypesRowTab(viewModel: MainViewModel = viewModel()) {
+    Row {
+        TabRow(
+            selectedTabIndex = viewModel.typesIndex,
+            backgroundColor = Color.Transparent,
+            contentColor = Color(0xFF149EE7)
+        ) {
+            viewModel.types.forEachIndexed { index, type ->
+                Tab(
+                    selected = index == viewModel.typesIndex,
+                    onClick = { viewModel.updateTypesIndex(index) },
+                    selectedContentColor = Color(0xFF149EE7),
+                    unselectedContentColor = Color(0xFF666666)
+                ) {
+                    Text(text = type, modifier = Modifier.padding(vertical = 8.dp), fontSize = 16.sp)
+                }
+            }
+        }
+    }
+}
+
+
+/*
+* 类型选择Tab
+* */
 @Preview
 @Composable
 fun StudyScreenPreview() {
--- app/src/main/java/com/linhos/wjycompose/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/linhos/wjycompose/viewmodel/MainViewModel.kt
@@ -2,11 +2,33 @@ package com.linhos.wjycompose.viewmodel
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.linhos.wjycompose.model.entity.Category
 
 class MainViewModel : ViewModel() {
-    val categorys =
-        mutableStateOf(listOf(Category("生活学习", Icons.Default.Person), Category("生活学习", Icons.Default.Person)))
+    val categorys by
+    mutableStateOf(
+        listOf(
+            Category("生活学习", Icons.Default.Person),
+            Category("生活学习", Icons.Default.Person)
+        )
+    )
+
+    var studyCategoryIndex by mutableStateOf(0)
+        private set
+
+    fun updateStudyCategoryIndex(index: Int) {
+        studyCategoryIndex = index
+    }
+
+    val types by mutableStateOf(listOf("文章", "视频"))
+    var typesIndex by mutableStateOf(0)
+        private set
+
+    fun updateTypesIndex(index: Int) {
+        typesIndex = index
+    }
 }
