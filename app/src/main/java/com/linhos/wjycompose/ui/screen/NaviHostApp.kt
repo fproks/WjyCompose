ad7b214392b27a9ff3c09c6f7ef4c3a5a775a897
fprok <fproks@126.com>
2023-04-02 21:53:45 +0800

    添加导航动画

--- app/src/main/java/com/linhos/wjycompose/ui/screen/NaviHostApp.kt
+++ app/src/main/java/com/linhos/wjycompose/ui/screen/NaviHostApp.kt
@@ -2,7 +2,9 @@ package com.linhos.wjycompose.ui.screen
 
 
 import android.util.Log
+import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.TweenSpec
 import androidx.compose.runtime.Composable
 import com.google.accompanist.navigation.animation.AnimatedNavHost
 import com.google.accompanist.navigation.animation.composable
@@ -12,19 +14,24 @@ import com.linhos.wjycompose.ui.navigation.Desionations
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun NaviHostApp() {
-    val  TAG = "NAVI_HOST"
+    val TAG = "NAVI_HOST"
     val navController = rememberAnimatedNavController()
     //带动画的导航
     AnimatedNavHost(navController = navController, startDestination = Desionations.HomeFrame.route) {
         Log.d("===", "st")
         //必须使用 com.google.accompanist.navigation.animation.composable 的 composable
-        composable(Desionations.HomeFrame.route) { //这个路由，跳转到这个里面的函数里
+        composable(Desionations.HomeFrame.route,
+            enterTransition = {
+                slideIntoContainer(AnimatedContentScope.SlideDirection.Up)//上滑进入
+            }) { //这个路由，跳转到这个里面的函数里
             MainFrame() {
                 Log.d(TAG, "navigate to MainFrame")
                 navController.navigate(Desionations.ArticleDetail.route)
             }
         }
-        composable(Desionations.ArticleDetail.route) { //这个路由，跳转到这里面来
+        composable(Desionations.ArticleDetail.route, enterTransition = {
+            slideIntoContainer(AnimatedContentScope.SlideDirection.Left)
+        }) { //这个路由，跳转到这里面来
             ArticleDetailScreen()
         }
 
