52b4754617ef84c0fa6db6f5299d243706a0196e
fproks <fproks@126.com>
2023-04-23 17:01:31 +0800

    创建login 界面

---
+++ app/src/main/java/com/linhos/wjycompose/ui/screen/Login.kt
@@ -0,0 +1,173 @@
+package com.linhos.wjycompose.ui.screen
+
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Password
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.linhos.wjycompose.R
+
+
+@Preview
+@Composable
+fun LoginScreen() {
+
+    var userName by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var showPassword by remember { mutableStateOf(false) }
+    BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.bg),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.linearGradient(
+                        listOf(Color(0xffbb8378), Color.Transparent),
+                        start = Offset(constraints.maxWidth.toFloat(), 0f),
+                        end = Offset(0f, constraints.maxHeight.toFloat())
+                    )
+                )
+        )
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.linearGradient(
+                        listOf(Color(0xff149EE7), Color.Transparent),
+                        start = Offset(0f, constraints.maxHeight.toFloat()),
+                        end = Offset(constraints.maxWidth.toFloat(), 0f)
+                    )
+                )
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = "用户登录",
+                fontSize = 20.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                TextField(value = userName, onValueChange = { userName = it }, leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = null,
+                        tint = Color.White
+                    )
+                }, singleLine = true, label = {
+                    Text(text = "用户名", color = Color.White, fontSize = 14.sp)
+                }, colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    focusedIndicatorColor = Color.LightGray,
+                    unfocusedIndicatorColor = Color.LightGray,
+                    focusedLabelColor = Color.LightGray,
+                    unfocusedLabelColor = Color.LightGray,
+                    cursorColor = Color.White
+                )
+                )
+                TextField(value = password,
+                    onValueChange = { password = it },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Password,
+                            contentDescription = null,
+                            tint = Color.White
+                        )
+                    },
+                    singleLine = true,
+                    label = {
+                        Text(text = "password", color = Color.White, fontSize = 14.sp)
+                    },
+                    visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = Color.Transparent,
+                        focusedIndicatorColor = Color.LightGray,
+                        unfocusedIndicatorColor = Color.LightGray,
+                        focusedLabelColor = Color.LightGray,
+                        unfocusedLabelColor = Color.LightGray,
+                        cursorColor = Color.White
+                    ),
+                    trailingIcon = {
+                        Icon(imageVector = if (showPassword) Icons.Default.VisibilityOff else Icons.Default.Visibility,
+                            contentDescription = null,
+                            modifier = Modifier.clickable {
+                                showPassword = !showPassword
+                            })
+                    })
+                Spacer(modifier = Modifier.height(20.dp))
+                val context =LocalContext.current
+                Text(
+                    text = "登  录",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .clickable {
+
+                            Toast.makeText(
+                                context,
+                                "已登录，用户名 $userName 密码 $password",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        },
+                    textAlign = TextAlign.Center
+                )
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(text = "还没有账号，点击注册", color = Color.Gray, fontSize = 14.sp)
+            }
+        }
+    }
+}
